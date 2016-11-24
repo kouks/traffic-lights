669101ad38e549a5cda92f4f23c630d4af20d19b
Pavel Koch <pavel.koch@wrongware.cz>
2016-11-24 10:13:24 +0000

    Transitions

--- helpers/asip.rkt
+++ helpers/asip.rkt
@@ -14,7 +14,16 @@
   off
   out
   in
+  transitions
+  transition
+  call
+  current
+  total-transition-time
+  timer
 )
+
+(define timer 0)
+(define current 0)
 
 (define (on? pin)
   (equal? (digital-read pin) HIGH)
@@ -38,4 +47,28 @@
 
 (define (in pin)
   (set-pin-mode pin INPUT_PULLUP_MODE)
+)
+
+(define transitions (list))
+(define (transition callback sleep)
+  (set! transitions (append transitions (list (list callback sleep))))
+)
+
+(define (total-transition-time all)
+  (cond
+    [(null? (cdr all)) (cadar all)]
+    [#t (+ (cadar all) (total-transition-time (cdr all)))]
+  )
+)
+
+(define (call)
+  (set! timer (+ timer 1))
+  (set! current 0)
+
+  (let ([state (modulo timer (total-transition-time transitions))])
+    (for ([t transitions] #:break (< state current))
+      (set! current (+ current (cadr t)))
+      (if (< state current) ((car t)) (void))
+    )
+  )
 )
--- main.rkt
+++ main.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang racket/gui
 
 ; Call the necessary API
 (require "asip/AsipMain.rkt")
@@ -37,13 +37,47 @@
 (on 5)
 (on 4)
 
-; Define the loop and run its
-(define (loop)
 
-  (loop)
+; Setting up stuff
+(define (tl1 color)
+  (cond
+    [(equal? color 'green) 11]
+    [(equal? color 'amber) 12]
+    [(equal? color 'red) 13]
+  )
 )
 
-(loop)
+(define (tl2 color)
+  (cond
+    [(equal? color 'green) 10]
+    [(equal? color 'amber) 9]
+    [(equal? color 'red) 8]
+  )
+)
+
+(transition (lambda ()
+  (on (tl1 'green))
+  (on (tl2 'red))
+  (off (tl2 'green))
+  (off (tl1 'red))
+) 30)
+
+(transition (lambda ()
+  (on (tl2 'amber))
+  (on (tl1 'amber))
+  (off (tl1 'green))
+  (off (tl2 'red))
+) 30)
+
+(transition (lambda ()
+  (on (tl2 'green))
+  (on (tl1 'red))
+  (off (tl1 'amber))
+  (off (tl2 'amber))
+) 30)
+
+; Define the loop and run it
+(define loop (new timer% [interval 100] [notify-callback call]))
 
 ; Close the connection
-(close-asip)
+; (close-asip)
