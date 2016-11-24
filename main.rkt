e32106f5ac466e40421fe9abd95b84b180f81431
Patryk Zatorski <patrykzatorski@patryks-mbp.uni.mdx.ac.uk>
2016-11-24 10:46:47 +0000

    Traffic lights states

--- main.rkt
+++ main.rkt
@@ -35,32 +35,58 @@
 
 (define (tl2 color)
   (cond
-    [(equal? color 'green) 10]
+    [(equal? color 'green) 8]
     [(equal? color 'amber) 9]
-    [(equal? color 'red) 8]
+    [(equal? color 'red) 10]
   )
 )
 
+(define (pc1 color)
+  (cond
+    [(equal? color 'green) 6]
+    [(equal? color 'red) 7]
+  )
+)
+
+(define (pc2 color)
+  (cond
+    [(equal? color 'green) 4]
+    [(equal? color 'red) 5]
+  )
+)
+
+;Traffic 1;
 (transition (lambda ()
+  (on (pc1 'red))
+  (on (pc2 'red))
   (on (tl1 'green))
   (on (tl2 'red))
-  (off (tl2 'green))
   (off (tl1 'red))
-) 30)
-
-(transition (lambda ()
-  (on (tl2 'amber))
-  (on (tl1 'amber))
-  (off (tl1 'green))
-  (off (tl2 'red))
-) 30)
-
-(transition (lambda ()
-  (on (tl2 'green))
-  (on (tl1 'red))
   (off (tl1 'amber))
   (off (tl2 'amber))
-) 30)
+
+) 40)
+
+(transition (lambda ()
+  (off (tl1 'green))
+  (on (tl1 'amber))
+  (on (tl2 'amber))
+) 20)
+
+(transition (lambda ()
+  (on (tl1 'red))
+  (off (tl1 'amber))
+  (on (tl2 'green))
+  (off (tl2 'amber))
+  (off (tl2 'red))
+) 40)
+
+(transition (lambda ()
+  (on (tl1 'amber))
+  (off (tl2 'green))
+  (on (tl2 'amber))
+) 20)
+
 
 ; Define the loop and run it
 (define loop (new timer% [interval 100] [notify-callback call]))
